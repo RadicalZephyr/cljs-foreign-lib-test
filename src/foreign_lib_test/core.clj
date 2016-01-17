4885396636f8aaa28367a840170e1344322e915d
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-16 23:14:09 -0600

    Initial commit

---
+++ src/foreign_lib_test/core.clj
@@ -0,0 +1,7 @@
+(ns foreign-lib-test.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/foreign_lib_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns foreign-lib-test.core-test
+  (:require [clojure.test :refer :all]
+            [foreign-lib-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
